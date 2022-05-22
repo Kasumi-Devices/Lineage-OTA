fab8fc14e19490c7c67b560837bf65953c61637b
Beru Hinode <windowz414@1337.lgbt>
2022-05-22 10:49:23 +0000

    Break from being tied to CI variables and use internal exported vars
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 if [ "${KASUMI_BUILD_TYPE}" == "vanilla" ]; then
-    jsonfile="${ROM_DIR}/vendor/kasumiota/${device}.json"
+    jsonfile="${TOP}/vendor/kasumiota/${LINEAGE_BUILD}.json"
 else
-    jsonfile="${ROM_DIR}/vendor/kasumiota/${KASUMI_BUILD_TYPE}/${device}.json"
+    jsonfile="${TOP}/vendor/kasumiota/${KASUMI_BUILD_TYPE}/${LINEAGE_BUILD}.json"
 fi
 d=$(date +%Y%m%d)
-sha256=$(sha256sum ${finalzip_path} | cut -d ' ' -f 1)
-utc=$(grep ro.build.date.utc $(dirname ${finalzip_path})/system/build.prop | cut -d '=' -f 2)
-size=$(wc -c ${finalzip_path} | cut -d ' ' -f 1)
-url="https://github.com/${release_repo}/releases/download/${tag}/${zip_name}"
+sha256=$(sha256sum ${OUT}/Kasumi*-OFFICIAL*.zip | cut -d ' ' -f 1)
+utc=$(grep ro.build.date.utc ${OUT}/system/build.prop | cut -d '=' -f 2)
+size=$(wc -c ${OUT}/Kasumi*-OFFICIAL*.zip | cut -d ' ' -f 1)
+url="https://dl.ayokaacr.workers.dev/4:/${LINEAGE_BUILD}/$(basename ${OUT}/Kasumi*-OFFICIAL*.zip)"
 
 # This is where the magic happens
 
 echo -e "{\n  \"response\": [\n    {" > ${jsonfile}
 echo -e "      \"datetime\": \"${utc}\"," >> ${jsonfile}
-echo -e "      \"filename\": \"${zip_name}\"," >> ${jsonfile}
+echo -e "      \"filename\": \"$(basename ${OUT}/Kasumi*-OFFICIAL*.zip)\"," >> ${jsonfile}
 echo -e "      \"id\": \"${sha256}\"," >> ${jsonfile}
 echo -e "      \"romtype\": \"OFFICIAL\"," >> ${jsonfile}
 echo -e "      \"size\": \"${size}\"," >> ${jsonfile}
 echo -e "      \"url\": \"${url}\"," >> ${jsonfile}
-echo -e "      \"version\": \"1.0\"\n    }\n  ]\n}" >> ${jsonfile}
+echo -e "      \"version\": \"1.2\"\n    }\n  ]\n}" >> ${jsonfile}
