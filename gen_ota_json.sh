10bcc24e3360aa8f33c2b007f5b6e3f0f43fcc1b
Beru Hinode <windowz414@1337.lgbt>
2022-06-27 18:48:19 +0000

    gen_ota_json: Some little changes
    * Generalize ZIP name detection, detect unofficial builds and
      others as well.
    
    * Add a little line to remind who runs this to push OTA to also
      write a changelog.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -6,18 +6,20 @@ else
     jsonfile="${TOP}/vendor/kasumiota/${KASUMI_BUILD_TYPE}/${LINEAGE_BUILD}.json"
 fi
 d=$(date +%Y%m%d)
-sha256=$(sha256sum ${OUT}/Kasumi*-OFFICIAL*.zip | cut -d ' ' -f 1)
+sha256=$(sha256sum ${OUT}/Kasumi*.zip | cut -d ' ' -f 1)
 utc=$(grep ro.build.date.utc ${OUT}/system/build.prop | cut -d '=' -f 2)
-size=$(wc -c ${OUT}/Kasumi*-OFFICIAL*.zip | cut -d ' ' -f 1)
-url="https://dl.ayokaacr.workers.dev/4:/${LINEAGE_BUILD}/$(basename ${OUT}/Kasumi*-OFFICIAL*.zip)"
+size=$(wc -c ${OUT}/Kasumi*.zip | cut -d ' ' -f 1)
+url="https://dl.ayokaacr.net/4:/${LINEAGE_BUILD}/$(basename ${OUT}/Kasumi*.zip)"
 
 # This is where the magic happens
 
 echo -e "{\n  \"response\": [\n    {" > ${jsonfile}
 echo -e "      \"datetime\": \"${utc}\"," >> ${jsonfile}
-echo -e "      \"filename\": \"$(basename ${OUT}/Kasumi*-OFFICIAL*.zip)\"," >> ${jsonfile}
+echo -e "      \"filename\": \"$(basename ${OUT}/Kasumi*.zip)\"," >> ${jsonfile}
 echo -e "      \"id\": \"${sha256}\"," >> ${jsonfile}
 echo -e "      \"romtype\": \"OFFICIAL\"," >> ${jsonfile}
 echo -e "      \"size\": \"${size}\"," >> ${jsonfile}
 echo -e "      \"url\": \"${url}\"," >> ${jsonfile}
 echo -e "      \"version\": \"1.2\"\n    }\n  ]\n}" >> ${jsonfile}
+
+echo "OTA has been created. Please navigate to $TOP/vendor/kasumiota and write a changelog named $LINEAGE_BUILD.md!"
