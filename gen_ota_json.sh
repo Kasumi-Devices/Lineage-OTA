5d7ae05bb188f8bcbed50475eeb14a7f90302af0
Beru Hinode <windowz414@1337.lgbt>
2023-06-19 14:25:44 +0200

    gen_ota_json: Improve ZIP detection logic
    * Append detection of build type to avoid conflicts with other
      build types.
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- gen_ota_json.sh
+++ gen_ota_json.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ "${KASUMI_BUILD_TYPE}" == "gapps" ]; then
+    DECRYPTED_KASUMI_BUILD_TYPE=GApps
+elif [ "${KASUMI_BUILD_TYPE}" == "auroraoss" ]; then
+    DECRYPTED_KASUMI_BUILD_TYPE=AuroraOSS
+else
+    DECRYPTED_KASUMI_BUILD_TYPE=""
+fi
 
 if [ "${KASUMI_BUILD_TYPE}" == "vanilla" ]; then
     jsonfile="${TOP}/vendor/kasumiota/${LINEAGE_BUILD}.json"
@@ -6,16 +13,16 @@ else
     jsonfile="${TOP}/vendor/kasumiota/${KASUMI_BUILD_TYPE}/${LINEAGE_BUILD}.json"
 fi
 d=$(date +%Y%m%d)
-sha256=$(sha256sum ${OUT}/Kasumi*.zip | cut -d ' ' -f 1)
+sha256=$(sha256sum ${OUT}/Kasumi*${DECRYPTED_KASUMI_BUILD_TYPE}-${LINEAGE_BUILD}.zip | cut -d ' ' -f 1)
 utc=$(grep ro.build.date.utc ${OUT}/system/build.prop | cut -d '=' -f 2)
-size=$(wc -c ${OUT}/Kasumi*.zip | cut -d ' ' -f 1)
-url="https://dl.ayokaacr.net/4:/${LINEAGE_BUILD}/$(basename ${OUT}/Kasumi*.zip)"
+size=$(wc -c ${OUT}/Kasumi*${DECRYPTED_KASUMI_BUILD_TYPE}-${LINEAGE_BUILD}.zip | cut -d ' ' -f 1)
+url="https://dl.ayokaacr.net/4:/${LINEAGE_BUILD}/$(basename ${OUT}/Kasumi*${DECRYPTED_KASUMI_BUILD_TYPE}-${LINEAGE_BUILD}.zip)"
 
 # This is where the magic happens
 
 echo -e "{\n  \"response\": [\n    {" > ${jsonfile}
 echo -e "      \"datetime\": \"${utc}\"," >> ${jsonfile}
-echo -e "      \"filename\": \"$(basename ${OUT}/Kasumi*.zip)\"," >> ${jsonfile}
+echo -e "      \"filename\": \"$(basename ${OUT}/Kasumi*${DECRYPTED_KASUMI_BUILD_TYPE}-${LINEAGE_BUILD}.zip)\"," >> ${jsonfile}
 echo -e "      \"id\": \"${sha256}\"," >> ${jsonfile}
 echo -e "      \"romtype\": \"OFFICIAL\"," >> ${jsonfile}
 echo -e "      \"size\": \"${size}\"," >> ${jsonfile}
